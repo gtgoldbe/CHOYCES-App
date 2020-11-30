1dbe8cd0bcecc5d80dbe8f0c24aca5cabefe0ff1
mco1996 <71357668+mco1996@users.noreply.github.com>
2020-11-29 22:19:22 -0500

    Add files via upload

---
+++ About-Us Content/src/assets/javascript/hoverable-menu.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.Services-Dropdown')) {
+    var dropdowns = document.getElementsByClassName("Services-Dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
---
+++ About-Us Content/src/assets/javascript/menu.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("Navigation_ul");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
