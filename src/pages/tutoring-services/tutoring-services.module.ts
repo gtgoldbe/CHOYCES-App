e0056f05160b56d6549923c86b69f6d5c5eddec7
Gayatri_Asodekar <54652927+Gayatr12@users.noreply.github.com>
2020-11-30 06:56:25 -0600

    Add files via upload

---
+++ src/pages/tutoring-services/tutoring-services.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { TutoringServicesPage } from './tutoring-services';
+
+@NgModule({
+  declarations: [
+    TutoringServicesPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(TutoringServicesPage),
+  ],
+})
+export class TutoringServicesPageModule {}
