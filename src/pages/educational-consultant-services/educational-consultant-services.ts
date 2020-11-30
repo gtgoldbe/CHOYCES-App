04942806d457101377faf70e415d1b06b43dc84b
Gayatri_Asodekar <54652927+Gayatr12@users.noreply.github.com>
2020-11-30 07:00:16 -0600

    Add files via upload

---
+++ src/pages/educational-consultant-services/educational-consultant-services.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HomePage} from '../home/home';
+import {TutoringServicesPage} from '../tutoring-services/tutoring-services';
+import {OurTeamPage} from '../our-team/our-team';
+
+/**
+ * Generated class for the EducationalConsultantServicesPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-educational-consultant-services',
+  templateUrl: 'educational-consultant-services.html',
+})
+export class EducationalConsultantServicesPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EducationalConsultantServicesPage');
+  }
+  goHome(){
+    this.navCtrl.setRoot(HomePage);
+  }
+  
+  swipeEvent(e){
+    if(e.direction == 2){
+      this.navCtrl.setRoot(OurTeamPage);
+  }
+    else{
+      this.navCtrl.setRoot(TutoringServicesPage);
+    }
+  }
+
+}
